64aa059fe24cd5939aaa11af20132e2c99c8c8a8
Tim Jaanson <23724760+J71m@users.noreply.github.com>
2018-02-09 15:49:55 +0200

    basic clock

---
+++ script.js
@@ -0,0 +1,37 @@
+// Avalikud muutujad
+let clockContainer // = null
+
+
+window.onload = function () {
+  init()
+}
+
+function init () {
+  clockContainer = document.querySelector('#clock')
+  console.log(clockContainer)
+
+  startClock()
+
+  document.querySelector('#change-position').addEventListener("click", changePosition)
+}
+
+function changePosition() {
+    console.log(event)
+
+    clockContainer.style.left = Math.round(Math.random()*window.innerWidth) + 'px';
+    clockContainer.style.top = Math.round(Math.random()*window.innerHeight) + 'px';;
+}
+
+function startClock () {
+    updateClock();
+
+    window.setInterval(function () {
+        updateClock();
+    }, 1000)
+}
+
+function updateClock(){
+    const date = new Date()
+
+    clockContainer.innerHTML = date
+}
