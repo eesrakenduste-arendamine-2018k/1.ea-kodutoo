438125b4678f1c1cdcb9f018ba328cfa5b2434eb
Kärt Nigols <kart.nigols@gmail.com>
2018-02-09 15:41:29 +0200

    Clock in the middle

---
+++ clock.js
@@ -0,0 +1,31 @@
+// Avalikud muutujad
+let clockContainer // = null
+
+
+window.onload = function () {
+  init()
+}
+
+function init () {
+  clockContainer = document.querySelector('#clock')
+  console.log(clockContainer)
+
+  startClock()
+}
+
+
+function startClock () {
+  updateClock()
+
+  window.setInterval(function () {
+   updateClock()
+  }, 1000)
+}
+
+function updateClock () {
+  const date = new Date()
+  clockContainer.innerHTML = date
+}
+function getTime (){
+  updateClock.getHours ()
+}
