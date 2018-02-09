caec2a8f4ed82e3ed3c7b5ea4fa534d9e996d934
Tatjana Kuznetsova <14123583+kuznetsovatatjana@users.noreply.github.com>
2018-02-09 11:50:19 +0200

    muudan kella taustavärvi

--- clock.js
+++ clock.js
@@ -1,6 +1,6 @@
 // Avalikud muutujad
 let clockContainer // = null
-
+let changeColorButton
 
 window.onload = function () {
   init()
@@ -9,14 +9,33 @@ window.onload = function () {
 function init () {
   clockContainer = document.querySelector('#clock')
   console.log(clockContainer)
+  changeColorButton = document.querySelector('#change-color')
 
   startClock()
+  
+  changeColorButton.addEventListener('click', changeBackgroundColor)
+}
+function changeBackgroundColor () {
+	console.log('muudan värvi')
+	
+
+	const r = Math.round(Math.random() * 255)
+	const g = Math.round(Math.random() * 255)
+	const b = Math.round(Math.random() * 255)
+	
+	
+	clockContainer.style.backgroundColor = 'rgb('+ r + ', '+ g + ','+ b + ')'     //
 }
 
 function startClock () {
+	updateClock()
   window.setInterval(function () {
-    const date = new Date()
+	updateClock()
+  }, 1000)
+}
+
+function updateClock () {
+	   const date = new Date()
 
     clockContainer.innerHTML = date
-  }, 1000)
 }
