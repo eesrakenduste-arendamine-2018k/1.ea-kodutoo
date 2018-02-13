42d74b213359eca7c66fe041653a4e322e95305f
j71m <23724760+J71m@users.noreply.github.com>
2018-02-13 15:38:19 +0200

    värviline taust ja üldisem css

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 // Avalikud muutujad
 let clockContainer // = null
-
+let rgb = []
+let w = window.innerWidth
+let h = window.innerHeight
 
 window.onload = function () {
   init()
@@ -8,18 +10,36 @@ window.onload = function () {
 
 function init () {
   clockContainer = document.querySelector('#clock')
+  dateContainer = document.querySelector('#date')
   console.log(clockContainer)
 
   startClock()
 
   document.querySelector('#change-position').addEventListener("click", changePosition)
+
+  document.querySelector('#container').addEventListener('mousemove',
+  function(event) {
+      color(event);
+  });
+}
+
+function color(e){
+	r = Math.round(e.pageX/w * 255)
+	g = Math.round((h-e.pageY)/h * 255)
+	b = 160
+    rgb = [r, g, b]
+
+    document.body.style.backgroundColor = 'rgb(' + 
+    [rgb[0],rgb[1],rgb[2]].join(',') + ')';
+
+    console.log(e)
 }
 
 function changePosition() {
     console.log(event)
 
-    clockContainer.style.left = Math.round(Math.random()*window.innerWidth) + 'px';
-    clockContainer.style.top = Math.round(Math.random()*window.innerHeight) + 'px';;
+    dateContainer.style.left = Math.round(Math.random()*window.innerWidth) + 'px';
+    dateContainer.style.top = Math.round(Math.random()*window.innerHeight) + 'px';;
 }
 
 function startClock () {
@@ -32,6 +52,16 @@ function startClock () {
 
 function updateClock(){
     const date = new Date()
+    dateStyle(date)
+} 
 
-    clockContainer.innerHTML = date
+function dateStyle(date){
+    let time = date.getHours() +":"+("0"+date.getMinutes()).slice(-2)//+":"+("0"+date.getSeconds()).slice(-2)
+
+    const monthsNames = ["jaanuar", "veebruar", "märts", "aprill", "mai", "juuni", "juuli", "august", "september", "oktoober", "november", "detsember"]
+
+    let longDate = date.getDate()+". "+monthsNames[date.getMonth()]+" "+date.getFullYear()
+
+    clockContainer.innerHTML = time
+    dateContainer.innerHTML = longDate
 }
