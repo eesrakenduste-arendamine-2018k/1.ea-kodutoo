50fa8872675a7c87d3c6bbf5ad0245b069389cf3
Erik Enden <erikenden@gmail.com>
2018-02-15 17:02:15 +0200

    Language change, minor UI tweaks

--- js/clock.js
+++ js/clock.js
@@ -1,4 +1,5 @@
 let clockContainer
+let language = "est";
 
 window.onload = function () {
     init()
@@ -9,7 +10,12 @@ function init () {
     minutesContainer = document.querySelector('#minutes')
     secondsContainer = document.querySelector('#seconds')
     dayContainer = document.querySelector('#day')
+    dateDayContainer = document.querySelector('#dateDay')
+    dateMonthContainer = document.querySelector('#dateMonth')
+    dateYearContainer = document.querySelector('#dateYear')
+    langButton = document.querySelector('#langBut')
     console.log(clockContainer)
+    console.log(language)
 
     startClock()
 }
@@ -21,9 +27,13 @@ function startClock () {
         let minutes = date.getMinutes()
         let seconds = date.getSeconds()
         const day = date.getDay()
+        
         let dayName
+        let dateDay = date.getDate()
+        let dateMonth = date.getMonth()
+        let dateYear = date.getFullYear()
 
-        dayName = dayText(day)
+        dayName = dayText(day, language)
         if(hours < 10){
             hours = "0" + hours
         }
@@ -37,11 +47,37 @@ function startClock () {
         minutesContainer.innerHTML = minutes
         secondsContainer.innerHTML = seconds
         dayContainer.innerHTML = dayName
+        dateDayContainer.innerHTML = dateDay
+        dateMonthContainer.innerHTML = monthText(dateMonth, language)
+        dateYearContainer.innerHTML = dateYear
     }, 1000)
 }
 
-function dayText(dayNumber){
+function dayText(dayNumber, language){
     const daysEst = ["Pühapäev", "Esmaspäev", "Teisipäev", "Kolmapäev", "Neljapäev", "Reede", "Laupäev"]
+    const daysEng = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
     
-    return daysEst[dayNumber]
+    if (language == "est"){
+        return daysEst[dayNumber]
+    } else if (language == "eng"){
+        return daysEng[dayNumber]
+    } 
+}
+
+function monthText(monthNumber, language){
+    const monthsEst = ["jaanuar", "veebruar", "märts", "aprill", "mai", "juuni", "juuli", "august", "september", "november", "detsember"]
+    const monthsEng = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "November", "December"]
+    if (language == "est"){
+        return monthsEst[monthNumber]
+    } else if (language == "eng"){
+        return monthsEng[monthNumber]
+    } 
+}
+
+function changeLang(){
+    if (language == "est"){
+        language = "eng"
+    } else if (language == "eng"){
+        language = "est"
+    }
 }
