ac1dc7ababdcd9126f4ab1e731bd066d88797ce7
Kevin Kodasma <31840235+kodasma@users.noreply.github.com>
2018-02-18 23:30:31 +0200

    Add files via upload

--- clock.js
+++ clock.js
@@ -1,28 +1,58 @@
 // JavaScript source code
+let clockContainer
+let dateContainer
+let color
+
+
+window.onload = function() {
+    init()
+}
+
 function init() {
     clockContainer = document.querySelector('#clock')
     console.log(clockContainer)
-
     startClock()
+
+    dateContainer = document.querySelector('#date')
+    console.log(dateContainer)
+    currentDate()
 
     document.querySelector('#size-up').addEventListener('click', sizeUp)
     document.querySelector('#size-down').addEventListener('click', sizeDown)
+    document.querySelector('#hide-clock').addEventListener('click', hideClock)
+    document.querySelector('#change-color').addEventListener('click', changeColor)
+}
 
-    window.addEventListener('keypress', hideClock)
+function changeColor (event){
+    console.log(event)
+    if (clockContainer.style.color === "black" && dateContainer.style.color === "black") {
+        clockContainer.style.color = "white"
+        dateContainer.style.color = "white"
+    } else {
+        clockContainer.style.color = "black"
+        dateContainer.style.color = "black"
+    }
 }
 
 function hideClock(event) {
     console.log(event)
+    if (clockContainer.style.visibility === "hidden" && dateContainer.style.visibility === "hidden"){
+        clockContainer.style.visibility = "visible"
+        dateContainer.style.visibility = "visible"
+    } else {
+        clockContainer.style.visibility = "hidden"
+        dateContainer.style.visibility = "hidden"
+    }
 }
 
 function sizeUp() {
     console.log('+')
-    clockContainer.style.fontSize = 12 + 'px'
+    clockContainer.style.fontSize = '175px'
 }
 
 function sizeDown() {
     console.log('-')
-    clockContainer.style.fontSize = '5px'
+    clockContainer.style.fontSize = '125px'
 }
 
 function startClock() {
@@ -34,7 +64,23 @@ function startClock() {
 }
 
 function updateClock() {
-    const date = new Date()
+    let date = new Date()
 
-    clockContainer.innerHTML = date
+    let curHr = date.getHours()
+    let curMin = date.getMinutes()
+
+    curHr = (curHr < 10 ? "0" : "") + curHr
+    curMin = (curMin < 10 ? "0": "") + curMin
+
+    clockContainer.innerHTML = curHr + ":" + curMin
+}
+
+function currentDate() {
+    let date = new Date()
+    
+    let months = ['Jaanuar', 'veebruar', 'Märts', 'Aprill', 'Mai', 'Juuni', 'Juuli', 'August', 'September', 'Oktoober', 'November', 'Detsember']
+    let days = ['','Esmaspäev', 'Teisipäev', 'Kolmapäev', 'Neljapäev', 'Reede', 'Laupäev', 'Pühapäev']
+
+    dateContainer.innerHTML = days[date.getDay()] + " " + date.getDate() + ". " + months[date.getMonth()] + " " + date.getFullYear()
+
 }
