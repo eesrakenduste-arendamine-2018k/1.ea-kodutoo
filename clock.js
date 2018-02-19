4d846c62dd09ea2e7a251ea4d8e9059a48e701e2
Mihkel Haava <mhaava@tlu.ee>
2018-02-19 21:11:15 +0200

    kellavärk

--- clock.js
+++ clock.js
@@ -1,53 +1,52 @@
 // Avalikud muutujad
 let clockContainer // = null
-let suurus = 40
+let size = 40;
+let topMargin = 5;
 let date;
-let string="0:0:0";
+let string = "0:0:0";
+let weekdays = ["Esmaspäev", "Teisipäev", "Kolmapäev", "Neljapäev", "Reede", "Laupäev", "Pühapäev"];
+let months = ["Jaanuar","Veebruar","Märts","Aprill","Mai","Juuni","Juuli","August","Detsember","Oktoober","November","Detsember"];
 
 window.onload = function () {
-  init()
+  init();
 }
 
 function init () {
-  clockContainer = document.querySelector('#clock')
-  console.log(clockContainer)
+  clockContainer = document.querySelector('#clock');
+  console.log(clockContainer);
 
   startClock()
-  document.querySelector('#size-up').addEventListener('click', sizeUp)
-  document.querySelector('#size-down').addEventListener('click', sizeDown)
-  window.addEventListener('keypress', hideClock)
+  document.querySelector('#size-up').addEventListener('click', sizeUp);
+  document.querySelector('#size-down').addEventListener('click', sizeDown);
+  document.querySelector('#clockColor').addEventListener('input', changeColor);
 
 }
-
-function hideClock(event) {
-	console.log(event)
-	if (event.key == 'h') {
-		clockContainer.style.display = 'none'
-	}
+function changeColor() {
+	document.getElementById("clock").style.color = document.getElementById("clockColor").value;
 }
 
 function sizeUp() {
-	suurus= suurus+5
-	console.log('+')
-	clockContainer.style.fontSize = suurus+'px'
+	size= size+5;
+	topMargin= topMargin+5;
+	console.log('+');
+	clockContainer.style.fontSize = size+'px';
+	clockContainer.style.marginTop = topMargin+'px';
 }
 function sizeDown() {
-	suurus= suurus-5
-	console.log('-')
-	clockContainer.style.fontSize = suurus+'px'
+	size= size-5;
+	topMargin= topMargin-5;
+	console.log('-');
+	clockContainer.style.fontSize = size+'px';
+	clockContainer.style.marginTop = topMargin+'px';
 }
 
-function startClock () {
-  updateClock()
+function startClock() {
+  updateClock();
 
-  window.setInterval(function () {
-   updateClock()
-  }, 1000)
-}
+  window.setInterval(function () { updateClock(); }, 1000)};
 
 function updateClock () {
-date= new Date()
-
-string= ('0' + date.getHours()).slice(-2)+":"+('0' + date.getMinutes()).slice(-2)+":"+('0' + date.getSeconds()).slice(-2)
-  clockContainer.innerHTML = string;
+	date= new Date();
+	string = ('0' + date.getHours()).slice(-2)+":"+('0' + date.getMinutes()).slice(-2)+":"+('0' + date.getSeconds()).slice(-2)+ "<br>" + weekdays[date.getDay()]+ " - " + date.getDate() + ". " + months[date.getMonth()];
+	clockContainer.innerHTML = string;
 }
