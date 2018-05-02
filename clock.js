593d7b50111eb9856c9c62bb102a3dea429a4fa2
Sigrid Kimask <kimasigr@users.noreply.github.com>
2018-05-02 16:27:12 +0300

    Lisan algfailid

---
+++ clock.js
@@ -0,0 +1,41 @@
+// Avalikud muutujad
+let clockContainer // = null
+let changeColorButton
+
+
+window.onload = function () {
+  init()
+}
+
+function init () {
+  clockContainer = document.querySelector('#clock')
+  console.log(clockContainer)
+  changeColorButton = document.querySelector('#change-color')
+  
+  startClock()
+  
+  changeColorButton.addEventListener('click', changeBackgroundColor)
+}
+
+function changeBackgroundColor () {
+	console.log('muudan värvi')
+	
+	const r = Math.round(Math.random() * 255)
+	const g = Math.round(Math.random() * 255)
+	const b = Math.round(Math.random() * 255)
+	clockContainer.style.backgroundColor = 'rgb('+ r +', '+ g +', '+ b +')'
+}
+
+function startClock () {
+	updateClock
+
+  window.setInterval(function () {
+    const date = new Date()
+
+    clockContainer.innerHTML = date
+  }, 1000)
+  
+}
+function updateClock () {
+	
+}
