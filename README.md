71c1433b884cb98ff942af96c16f656e0c7e19ce
Dennis Richard Šulga <desulg@users.noreply.github.com>
2018-05-17 18:13:00 +0300

    Muudetud kell

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 Dennis Richard Šulga
 
-Rakendus näitab kella ja kuupäev ning kasutab kella numbrilist järjestust heksadetsimaal väärtuses, mille seab ta lehe tausta värvi atribuudile. Samuti võtab ta selle sama värvi koodi ja keerab tagurpidi, kuid sekundid jätab samas järjestuses - see võimaldab lehel kiiremini taustavärvi muuta, kuid kui keerata ka sekundid tagurpidi, siis hakkavad värvid aina kiiremini vahetuma, mitte nii sujuvalt. 
-Kui viia hiir tekstiridade peale, siis read vahetavad värvi valgeks ning suurenevad. 
-Samuti eelpool mainitud värvikoodid on ka lehel nähtavad ja ka nende muutused. Sekundite arv on otseselt seotud selle kahe rea fonti suurusega. Et nende kasv ei tunduks nii ebameeldivalt diskreetne, siis ka nende vahel on üleminek sujuvamaks tehtud.
+Rakendus näitab reaalajas kella. 
+*Tehtud kaks nuppu, mille peale on kirjas nende tegevused(taustavärvi vahetus ja kella asukoha muut). Sealjuures mõlemad kasutavad random funktsioone, et parameetreid valida
+*Kui vajutada kuupäevale, siis muutub teksti värv
+*Vajutades kella peale, muutub teksti font
+*Samuti on all välja toodud link, mille peale hiirekursorit viies, suureneb see 2 sekundilise viitega.
--- clock.js
+++ clock.js
@@ -1,65 +1,92 @@
-var clock = document.getElementById('clock');
-var dayDate = document.getElementById('dayDate');
-var hexColor = document.getElementById('hex-color');
-var backColor = document.getElementById('back-color');
+let clockContainer 
+let dateContainer 
 
-function hexClock() {
-    var time = new Date();
-    var hours = time.getHours().toString();
-    var minutes = time.getMinutes().toString();
-    var seconds = time.getSeconds().toString();
-    var date = time.getDate().toString();
-    var dayNr = time.getDay().toString();
-    var monthNr = time.getMonth().toString();
-
-    if (hours.length < 2) {
-        hours = '0' + hours;
-    }
-
-    if (minutes.length < 2) {
-        minutes = '0' + minutes;
-    }
-
-    if (seconds.length < 2) {
-        seconds = '0' + seconds;
-    }
-
-    
-    var listOfDays = ["Esmaspäev", "Teisipäev", "Kolmapäev", "Neljapäev",
-     "Reede", "Laupäev", "Pühapäev"];
-    day = listOfDays[dayNr];
-
-    var listOfMonths = ["Jaanuar", "Veebruar", "Märts", "Aprill",
-     "Mai", "Juuni", "Juuli", "August", "September", "Oktoober", "November",
-     "Detsember"];
-    month = listOfMonths[monthNr];
-
-function reverseString(str) {
-    return str.split("").reverse().join("");
+window.onload = function () {
+	init()
 }
 
-    var clockStr = hours + ' : ' + minutes + ' : ' + seconds;
-    var weekdayDateStr = day + " " + date + ". " + month;
-
-    var hexColorStr = '#' + hours + minutes + seconds;
-    var hexNr_hm = hours + minutes;
-    var hexColStrRev = '#'+ seconds + reverseString(hexNr_hm); 
-
-    var clockSize = seconds + 'px';
-    
-
-    clock.textContent = clockStr;
-    dayDate.textContent = weekdayDateStr;
-    
-    
-    hexColor.textContent = "Paragraph color: " + hexColStrRev;
-    backColor.textContent = "Background color: " + hexColorStr;
-    
-    document.body.style.backgroundColor = hexColorStr;
-    document.body.style.color = hexColStrRev;
-    document.getElementById("colorInfo").style.fontSize = clockSize;
-    
+function init () {
+	clockContainer = document.querySelector('#clock')
+	console.log(clockContainer)
+	dateContainer = document.querySelector('#date')
+	console.log(dateContainer)
+	 
+	startClock()
+	todaysDate()
 }
 
-hexClock();
-setInterval(hexClock, 1000);
+
+function startClock () {
+	updateClock()
+
+	window.setInterval(function () {
+	updateClock()
+	}, 1000)
+}
+
+function updateClock () {
+	const time = new Date();
+	var hours = (time.getHours()).toString();
+	var minutes = time.getMinutes().toString();
+	var seconds = time.getSeconds().toString();
+	if (hours.length < 2) {
+		hours = '0' + hours;
+	}
+
+	if (minutes.length < 2) {
+		minutes = '0' + minutes;
+	}
+
+	if (seconds.length < 2) {
+		seconds = '0' + seconds;
+	}
+		  
+	clockContainer.innerHTML = hours + ' : ' + minutes + ' : ' + seconds;
+}
+
+function todaysDate(){
+	const time = new Date();
+	var day = time.getDate();
+	var month = time.getMonth();
+	var year = time.getFullYear();
+	var weekday = time.getDay();
+	var weekdays = ['Pühapäev', 'Esmaspäev', 'Teisipäev', 'Kolmapäev', 'Neljapäev', 'Reede', 'Laupäev'];
+	var todaysday = weekdays[weekday];
+	
+	dateContainer.innerHTML = todaysday + ",  " + day + "/" + (month+1) + "/" + year;
+}
+
+ 
+function changeColor(){
+	var random= Math.floor(Math.random() * 6);  
+	var colors = ["red", "black", "gray", "blue", "cyan", "magenta"];
+    document.getElementById("bigPic").style.color=colors[random];
+    document.getElementById("clock").style.color=colors[random];
+}
+
+function changeBgCol(){
+    var random= Math.floor(Math.random() * 5);  
+    var colors = ["red", "black", "gray", "blue", "cyan", "magenta"];
+    document.body.style.backgroundColor = colors[random]
+}
+
+function changeFont() {
+	var random= Math.floor(Math.random() * 4);
+	var fonts = ["Comic Sans MS", "Georgia", "FreeMono", "Courier New"];
+    document.getElementById("clock").style.fontFamily=fonts[random];
+    document.getElementById("bigPic").style.fontFamily=fonts[random];
+}
+
+function changeClockPos(){
+	var random = Math.floor(Math.random() * 4);
+	var pos = ["left", "right", "center", "justify"]
+	document.body.style.textAlign = pos[random]
+}
+
+/*Kasutatud maretjal: 
+	https://websitesetup.org/web-safe-fonts-html-css/
+	https://www.sitepoint.com/community/t/code-to-enlarge-webpage-content-font-size/70260/5
+	https://stackoverflow.com/questions/26652595/javascript-onclick-text-size-increase-with-eventlistener
+	https://css-tricks.com/forums/topic/how-to-change-text-color-when-clicking-in-another-div/
+	https://codepen.io/rm89/pen/aNOmzQ
+*/
