bcb8a35ccce098121688beb61c3d5067d0546e50
Robert Roos <31948767+roosen7@users.noreply.github.com>
2018-05-28 17:08:46 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,78 @@
+var clock = document.getElementById('clock');
+var hexColor = document.getElementById('hex-color');
+
+//Kell 
+function hexClock() {
+  var time = new Date();
+  var hours = time.getHours().toString();
+  var minutes = time.getMinutes().toString();
+  var seconds = time.getSeconds().toString();
+
+  if (hours.length < 2) {
+    hours = '0' + hours;
+  }
+
+  if (minutes.length < 2) {
+    minutes = '0' + minutes;
+  }
+
+  if (seconds.length < 2) {
+    seconds = '0' + seconds;
+  }
+
+  var clockStr = hours + ' : ' + minutes + ' : ' + seconds;
+  var hexColorStr = '#' + hours + minutes + seconds;
+
+  clock.textContent = clockStr;
+  hexColor.textContent = hexColorStr;
+  document.body.style.backgroundColor = hexColorStr;
+}
+
+function colorClock() {
+	var colors = ['#FF6633', '#FFB399', '#FF33FF', '#FFFF99', '#00B3E6', 
+		  '#E6B333', '#3366E6', '#999966', '#99FF99', '#B34D4D',
+		  '#80B300', '#809900', '#E6B3B3', '#6680B3', '#66991A', 
+		  '#FF99E6', '#CCFF1A', '#FF1A66', '#E6331A', '#33FFCC',
+		  '#66994D', '#B366CC', '#4D8000', '#B33300', '#CC80CC', 
+		  '#66664D', '#991AFF', '#E666FF', '#4DB3FF', '#1AB399',
+		  '#E666B3', '#33991A', '#CC9999', '#B3B31A', '#00E680', 
+		  '#4D8066', '#809980', '#E6FF80', '#1AFF33', '#999933',
+		  '#FF3380', '#CCCC00', '#66E64D', '#4D80CC', '#9900B3', 
+		  '#E64D66', '#4DB380', '#FF4D4D', '#99E6E6', '#6666FF'];
+//Valib suvalise värvi
+	var random_color = colors[Math.floor(Math.random() * colors.length)];
+	document.getElementById('clock').style.color = random_color;
+}
+//Reset tagasi valgeks
+function resetClock(){
+	document.getElementById('clock').style.color = 'white';
+}
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+//funktsioon slaidide jaoks
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1} 
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none"; 
+    }
+    for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+    }
+  slides[slideIndex-1].style.display = "block"; 
+  dots[slideIndex-1].className += " active";
+}
+
+hexClock();
+setInterval(hexClock, 1000);
